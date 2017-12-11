fad1e5a5c90eaa686c7d0076a194787b93440768
neretin-trike <hawktrike@gmail.com>
2017-12-11 21:50:59 +0700

    Изменил файловую структуру проекта

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     less = require('gulp-less');
 
 gulp.task('less', function(){ // Создаем таск "less"
-    return gulp.src('app/less/container/container.less') // Берем источник
+    return gulp.src('app/less/*.less') // Берем источник
         .pipe(less()) // Преобразуем less в CSS посредством gulp-less
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 });
