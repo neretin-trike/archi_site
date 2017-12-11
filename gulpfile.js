ee66cfce9950047ba775cc1478ea24af16a8d2f9
neretin-trike <hawktrike@gmail.com>
2017-12-11 22:11:26 +0700

    Добавил автоматическое слежение за файлами; добавил плагин browser-sync, который обновляет страницу браузера

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    browserSync = require('browser-sync'); // Подключаем Browser Sync
 
 gulp.task('less', function(){ // Создаем таск "less"
     return gulp.src('app/less/*.less') // Берем источник
         .pipe(less()) // Преобразуем less в CSS посредством gulp-less
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browser Sync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'less'], function() {
+    gulp.watch('app/less/*.less', ['less']); // Наблюдение за sass файлами
+    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
 });
